fc1d8323bc25df710cf9ac176e99ddfc2e2bd4ec
Hao Xu <xuhao6815@gmail.com>
2024-10-11 19:42:03 +0800

    upload

---
+++ build/ur_simulation_gz/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/wx_pc/Desktop/ur_sim/src/Universal_Robots_ROS2_GZ_Simulation/ur_simulation_gz
+# Build directory: /home/wx_pc/Desktop/ur_sim/build/ur_simulation_gz
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
